9c1fad1070366fdb62e25a50a8f48afeb87bf9ff
Roshan Panta <roshanpanta2059@gmail.com>
2023-04-07 20:01:10 +0545

    added taking_ input

---
+++ taking_input.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+print("The addition result is:" ,( a + b))
+print("The subtraction result is:" ,( a - b))
+print("The multiplication result is:" ,( a * b))
+print("The division result is:" ,( a / b))
+print("The reminder is:", (a % b))
