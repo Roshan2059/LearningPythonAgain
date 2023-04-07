87045ca497665bea9b96373e96fcfa7b9ff844c2
Roshan Panta <roshanpanta2059@gmail.com>
2023-04-07 19:14:59 +0545

    added the exercise

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+
+print("The addition result is:" ,( a + b))
+print("The subtraction result is:" ,( a - b))
+print("The multiplication result is:" ,( a * b))
+print("The division result is:" ,( a / b))
+print("The reminder is:", (a % b))
