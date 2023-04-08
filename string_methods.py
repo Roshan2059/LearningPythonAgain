5b7c48ef0176ad926bda224e7e9f4d0b4aeafb18
Roshan Panta <roshanpanta2059@gmail.com>
2023-04-08 13:22:55 +0545

    added string_methods

---
+++ string_methods.py
@@ -0,0 +1,30 @@
+a = "Roshan"
+print(len(a))
+print(a.upper())
+print(a.lower())
+nm = "roshan"
+print(nm.capitalize())
+nm = "roShaN"
+print(nm.capitalize())
+nm = "roshan!!!###"
+print(nm.rstrip("#!"))
+print(nm.replace("!#", "roshan"))
+print(nm.split("n"))
+print(nm.count("!#"))
+print(nm.endswith("!"))
+# print(nm.center)
+print(nm.find("n"))
+print(nm.islower())
+print(nm.isalnum())
+print(nm.isalpha())
+print(nm.islower())
+print(nm.isprintable())
+print(nm.isspace())
+print(nm.istitle())
+
+hello = "Roshan Panta"
+print(hello.istitle())
+hello = "Roshan panta"
+print(hello.istitle())
+print(hello.startswith("Roshan"))
+print(hello.startswith("roshan"))
