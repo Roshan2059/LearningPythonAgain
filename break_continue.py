880da18417c795f63c7a63aea2c3609e187738dc
Roshan Panta <roshanpanta2059@gmail.com>
2023-04-09 12:32:34 +0545

    addded

--- break_continue.py
+++ break_continue.py
@@ -2,3 +2,8 @@ for i in range(10):
     if(i==5):
         break
     print("2 X ", i+1, " = ", 2*(i+1))
+
+for i in range(10):
+    if(i==5):
+        continue
+    print("2 X ", i+1, " = ", 2*(i+1))
