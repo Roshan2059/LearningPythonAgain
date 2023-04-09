fde9166e7fa8785a1cb44d1a25de93cc1313e1e6
Roshan Panta <roshanpanta2059@gmail.com>
2023-04-09 12:13:29 +0545

    added the for loop

---
+++ for_loop.py
@@ -0,0 +1,16 @@
+name = "Roshan"
+for i in name:
+    print(i)
+
+colors = ["red", "yellow", "Greeen", "blue"]
+for color in colors:
+    print(color)
+
+for i in range(10):
+    print(i)
+
+for i in range(1, 11):
+    print(i)
+
+for i in range(1, 101 ,9):
+    print(i)
