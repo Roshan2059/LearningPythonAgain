2c9b964f439d9160ed3658fcec733a71641438e8
Roshan Panta <roshanpanta2059@gmail.com>
2023-04-09 08:12:17 +0545

    added match case

---
+++ match_case.py
@@ -0,0 +1,17 @@
+x =  int(input("Enter your age:"))
+
+match x:
+    case 16:
+        print("Wow!!!! you are a sweet sixteen")
+    case 20:
+        print("jindagi ma dukha gar hai vai natra dukha pauxas")
+    case 28:
+        print("Bihey garni bela vayena ra?")
+    case 30:
+        print("bihe garya 2 barsa vaisako janmauna pardaina??")
+    case 40:
+        print("2 crore ko jati assets/cash xa vaney aba retire garda hunxa")
+    case 60:
+        print("hello sarkari jagirey! retire vayeko ma bahdai xa!")
+    case _:
+        print("Kasto aadey umer ho yaar tapaiko")
